36055723a67fd8fd08dcbd8e41cab7e206374a05
varajala <valtteri.rajalainen@gmail.com>
2021-12-09 16:24:05 +0200

    Added warning if EMAIL_CREEDE

--- blog/config.py
+++ blog/config.py
@@ -2,18 +2,15 @@ import sys
 import os
 
 
+# For testing
 EMAIL_HOST = (None, sys.stdout)
+
+# For actually sending emails
 # EMAIL_HOST = ('smtp.gmail.com', 465),
+
 EMAIL_USE_SSL = True
 EMAIL_CREDENTIALS = os.path.join(os.path.dirname(__file__), 'email-credentials')
 
 DATABASE = os.path.join(os.path.dirname(__file__), 'database.db')
 
 SECRET_KEY = 'development'
-
-
-if not os.path.exists(EMAIL_CREDENTIALS):
-    with open(EMAIL_CREDENTIALS, 'wb') as file:
-        file.write(b'email-address')
-        file.write(b'\n')
-        file.write(b'password')
